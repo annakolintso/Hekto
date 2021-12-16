408a2818ae715b3de4b21a62cdc4f7a62a7164c1
Anna Kolintso <anna.kolintso@gmail.com>
2021-12-16 11:51:25 +0200

    Updates

--- src/js/signup.js
+++ src/js/signup.js
@@ -1,4 +1,4 @@
-import "./../style/signup.scss";
+import './../style/signup.scss';
 
 const form = document.querySelector('.signup__form');
 const inputs = form.querySelectorAll('.form-group__input');
@@ -6,74 +6,82 @@ const successText = document.querySelector('.signup__success');
 
 
 const validationField = (el) => {
-    const formGroup = el.closest('.form-group');
-    let result = true;
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    const invalidEmail = el.getAttribute('name') === 'email' && el.value && !re.test(el.value);
+  const formGroup = el.closest('.form-group');
+  let result = true;
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const isEmail = el.getAttribute('name') === 'email';
+  const invalidEmail = isEmail && el.value && !re.test(el.value);
 
-    formGroup.classList.remove('has-error');
-    formGroup.classList.remove("required");
-    formGroup.classList.remove("pattern");
+  formGroup.classList.remove('has-error');
+  formGroup.classList.remove('required');
+  formGroup.classList.remove('pattern');
 
-    if (el.value === '' || invalidEmail) {
-        formGroup.classList.add('has-error');
-        formGroup.classList.add(invalidEmail ? "pattern" : "required");
-        result = false;
-    }
-    return result;
-}
+  if (el.value === '' || invalidEmail) {
+    formGroup.classList.add('has-error');
+    formGroup.classList.add(invalidEmail ? 'pattern' : 'required');
+    result = false;
+  }
+  return result;
+};
 
 const validation = () => {
-    let countError = 0;
-    inputs.forEach(el => {
-        let validateField = validationField(el);
-        !validateField && countError++;
-    })
-    return !countError;
-}
+  let countError = 0;
+  inputs.forEach((el) => {
+    const validateField = validationField(el);
+    !validateField && countError++;
+  });
+  return !countError;
+};
 
 const getFormData = () => {
-    const data = {};
-    inputs.forEach(el => {
-        const key = el.name;
-        const value = el.value;
-        data[key] = value;
-    })
-    return data;
-}
+  const data = {};
+  inputs.forEach((el) => {
+    const key = el.name;
+    const value = el.value;
+    data[key] = value;
+  });
+  return data;
+};
 
 const clearForm = () => {
-    inputs.forEach(el => el.value = '');
-}
+  inputs.forEach((el) => el.value = '');
+};
 
-inputs.forEach(el => {
-    el.addEventListener('blur', () => {
-        validationField(el);
-    })
-})
+const setCookie = (cname, cvalue, exdays) => {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  const expires = 'expires=' + d.toUTCString();
+  document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
+};
+
+inputs.forEach((el) => {
+  el.addEventListener('blur', () => {
+    validationField(el);
+  });
+});
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const validated = validation();
-    const formData = getFormData();
-    if (validated) {
-        fetch('https://jsonplaceholder.typicode.com/posts', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json;charset=utf-8'
-            },
-            body: JSON.stringify(formData)
-          })
-          .then(() => {
-            document.cookie = 'formData=' + JSON.stringify(formData);
-            successText.classList.add('active');
-            setTimeout(() => {
-                successText.classList.remove('active');
-            }, 2000)
-            clearForm();
-          })
-          .catch(err => {
-              console.error(err);
-          })
-    }
-})
+  e.preventDefault();
+  const validated = validation();
+  const formData = getFormData();
+  if (validated) {
+    setCookie('formData', JSON.stringify(formData), 2);
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify(formData),
+    })
+        .then(() => {
+          successText.classList.add('active');
+          setTimeout(() => {
+            successText.classList.remove('active');
+          }, 2000);
+          clearForm();
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+  }
+});
